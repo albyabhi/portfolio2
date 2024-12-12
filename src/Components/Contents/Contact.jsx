249ba82ee908a411c17a07912e288aecdf0463a8
Alby <alby4645@gmail.com>
2024-12-12 09:57:31 +0530

    3 commit

--- src/Components/Contents/Contact.jsx
+++ src/Components/Contents/Contact.jsx
@@ -6,14 +6,18 @@ const Contact = () => {
     <div className="contact-container">
       <h2>Contact Information</h2>
       <div className="contact__details">
-        <p><strong>Phone:</strong> +91 7994908388</p>
+        <p><strong>Phone:</strong> <a href="tel:+917994908388">+91 7994908388</a></p>
         <p>
-          <strong>Email:</strong> 
+          <strong>Email:</strong>
           <a href="mailto:alis207702@gmail.com" aria-label="Send an email to alis207702@gmail.com">
             alis207702@gmail.com
           </a>
         </p>
-        <p><strong>Address:</strong> 123 Anywhere St., Any City</p>
+        <p>
+          <strong>Address:</strong> 
+          53 Ushakirana Nilaya, Anjanadri Layout, 
+           Achit Nagar PO, Thammanahalli, Bangalore
+        </p>
       </div>
     </div>
   );
